176ae20848586f7bdba3d5986e3cace8829a495c
pengwei03 <pengwei03@vcredit.com>
2019-11-12 16:57:24 +0800

    提交

---
+++ nacos-mybatisplus/src/main/java/com/vc/mybatisplus/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.vc.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.vc.mybatisplus.domain.User;
+
+/**
+ * User 表数据库控制层接口
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
